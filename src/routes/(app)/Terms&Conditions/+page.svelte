c9944f36c4d0b95abd1175348d214577b3db048a
Joannerazelle <Joannerazelle19@gmail.com>
2022-11-21 11:29:00 +0800

    Added Terms&condition from routes/app

--- src/routes/(app)/Terms&Conditions/+page.svelte
+++ src/routes/(app)/Terms&Conditions/+page.svelte
@@ -1,179 +1,109 @@
 <script>
   //@ts-nocheck
-  import { Icon } from 'svelte-materialify'
-  import { mdiCircleSmall } from '@mdi/js'
+  let innerWidth = 0
 </script>
 
+<svelte:window bind:innerWidth />
+
 <div class="hero-body p-0">
-  <div class="columns m-5 mr-5 is-flex is-justify-content-center is-multiline is-align-items-center">
+  <div class="columns m-5 mx-5 is-flex is-justify-content-center is-multiline is-align-items-center">
     <div class="column mt-0 is-align-items-center is-mobile">
-      <p class=" mt-4 mb-5 is-size-3-desktop is-size-3-tablet is-size-6-mobile is-primary has-text-centered fredokaone">
-        <span style="font-size: 150%;">
+      <p class=" mt-4 mb-5 is-primary has-text-centered fredokaone is-mobile {innerWidth < 571 ? 'txt-size-15' : 'txt-size-20'}">
           Terms and Conditions
-        </span>
       </p>
 
       <!-- Paragraphs/Texts about Axion --> 
-      <div class="dm-sans is-align-items-center is-mobile">       
-        <p class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile has-text-centered"> 
+      <div class="is-justify-content-center is-align-content-center">      
+        <p class="fredoka-reg has-text-centered {innerWidth < 571 ? 'txt-size-10' : 'txt-size-15'}"> 
           Welcome to Axion, an academic web-based task management system. We appreciate you coming here. In our terms and conditions, the “Service” refers to the application or website that you are interacting with, whereas ”Axion” serves as the system itself, including the database and so forth. (“we”, “our”, or “us”) refers to the developers. This policy applies worldwide and we are the only team that controls your data.
         </p>
         <br/>
-        <p class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile has-text-centered">
+        <p class="fredoka-reg has-text-centered {innerWidth < 571 ? 'txt-size-10' : 'txt-size-15'}">
           As we solemnly advise you to read this, we want to give you a note that it is our obligation to explain this policy in such detail. We want to ensure that acceptable behavior of using our Service is being conducted within the reach of Axion, for this policy will serve on behalf of Axion in case any future legal disputes may occur. That having been said, by reading this policy, you will understand how our rules cover our Service and revolve within Axion, as well as our <a class="hover-txt-style-underline" href="/Privacy_Policy" >Privacy policy</a>.
         </p>
       </div>   
 
       <br/>
 
-      <p class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile has-text-justified has-text-weight-bold">
-        The following topics are being addressed:
-      </p>
-      
-      <!-- type -->
-      <div class="dm-sans is-mobile is-hidden-mobile has-text-justified">
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          Limitation of liability
-        </Icon>
-        <br/>
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          Rules and conditions
-        </Icon>
-        <br/>
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          Feedback statement
-        </Icon>
-        <br/>
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          Further updates in our terms and conditions
-        </Icon>
-      </div>
 
-      <br/>
-
-      <div class="dm-sans is-mobile is-hidden-desktop is-hidden-tablet-only has-text-justified">
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          Limitation of liability
-        </Icon>
-        <br/>
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          Rules and conditions
-        </Icon>
-        <br/>
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          Feedback statement
-        </Icon>
-        <br/>
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          Further updates in our terms and conditions
-        </Icon>
+      <div class="content">
+        <p class="fredoka-reg has-text-centered {innerWidth < 571 ? 'txt-size-10' : 'txt-size-15'} has-text-justified has-text-weight-bold">
+          The following topics are being addressed:
+        </p>
+        <ul class="fredoka-reg {innerWidth < 571 ? 'txt-size-10' : 'txt-size-15'}">
+          <li>Limitation of liability</li>
+          <li>Rules and conditions</li>
+          <li>Feedback statement</li>
+          <li>Further updates in our terms and conditions</li>
+        </ul>
       </div>
 
       <br/>
       
-      <p class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile has-text-justified">
+      <p class="fredoka-reg has-text-justified {innerWidth < 571 ? 'txt-size-10' : 'txt-size-15'} ">
         By accessing and using our Service, you acknowledge that you have read, understand, and agree to be bound by our privacy policy. If you do not wish for your data to be collected, used, and disclosed by us for personal reasons and privacy concerns, please halt and discontinue using our Service.
       </p>
 
       <br/>
 
-      <p class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile has-text-justified has-text-weight-bold">
+      <p class="fredoka-reg has-text-justified {innerWidth < 571 ? 'txt-size-10' : 'txt-size-15'} has-text-weight-bold">
         Limitation of liability
       </p>
-      <p class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile has-text-justified">
+      <p class="fredoka-reg has-text-justified {innerWidth < 571 ? 'txt-size-10' : 'txt-size-15'}">
         We want to disclaim our liability for malicious attacks, or any form that could cause damage to your device. We do not intend, plan, and tolerate to have ads, nor viruses within our Service, thus in any case that your device is infected, our Service will not be suspected. Users sharing files that contain malicious code will be held accountable for any further damage it can cost to Axion, or to other users.
       </p>
 
       <br/>
-
-      <p class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile has-text-justified has-text-weight-bold">
+      
+      <div class="content">
+        <p class="fredoka-reg has-text-justified {innerWidth < 571 ? 'txt-size-10' : 'txt-size-15'} has-text-weight-bold">
         Furthermore, we warranty for not being responsible towards the following areas:
-      </p>
-      <div class="dm-sans is-hidden-mobile is-size-4-desktop is-size-3-tablet is-size-6-mobile has-text-justified">
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          Service downtime
-        </Icon>
-        <br/>
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          User satisfaction
-        </Icon>
-        <br/>
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          Errors or inaccuracies, such as typos
-        </Icon>
+        </p>
+        <ul class="fredoka-reg has-text-justified {innerWidth < 571 ? 'txt-size-10' : 'txt-size-15'}">
+          <li>Service downtime</li>
+          <li>User satisfaction</li>
+          <li>Errors or inaccuracies, such as typos</li>
+        </ul>
       </div>
-      <div class="dm-sans is-hidden-tablet is-hidden-desktop is-size-4-desktop is-size-3-tablet is-size-6-mobile has-text-justified">
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          Service downtime
-        </Icon>
-        <br/>
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          User satisfaction
-        </Icon>
-        <br/>
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          Errors or inaccuracies, such as typos
-        </Icon>
-      </div>
+
       <br/>
 
-      <p class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile has-text-justified has-text-weight-bold">
+      <p class="fredoka-reg has-text-justified {innerWidth < 571 ? 'txt-size-10' : 'txt-size-15'} has-text-weight-bold">
         Rules and conditions
       </p>
-      <p class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile has-text-justified">
+      <p class="fredoka-reg has-text-justified {innerWidth < 571 ? 'txt-size-10' : 'txt-size-15'}">
         Our service must only be used specifically for its intended purposes only, and not for the purpose of harm, fraudulence, discrimination, and such. We warrant that you are at least 18 years old and above, in which you will be permitted to access our Service. You must not distribute, sell, display, reproduce, disarrange, translate, and exploit Axion, and any portions of it, including the logo, images, design, textual information, etc. You will also agree not to engage in any inappropriate, illegal, violent, and harmful actions or behavior that will negatively affect the performance of Axion and/or other users. The said further actions or behavior can be the following, but are not limited to:
       </p>
 
-      <br/>
-        
-      <div class="dm-sans is-hidden-mobile is-size-4-desktop is-size-3-tablet is-size-6-mobile has-text-justified">
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          Uploading malicious code
-        </Icon>
-        <br/>
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          Accessing data that you are not allowed to access
-        </Icon>
-        <br/>
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          Sharing or displaying inappropriate, misleading information within Axion
-        </Icon>
-      </div>
-      <div class="dm-sans is-hidden-tablet is-hidden-desktop is-size-4-desktop is-size-3-tablet is-size-6-mobile has-text-justified">
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          Uploading malicious code
-        </Icon>
-        <br/>
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          Accessing data that you are not allowed to access
-        </Icon>
-        <br/>
-        <Icon size='50px' path={mdiCircleSmall} class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile" >
-          Sharing or displaying inappropriate, misleading information within Axion
-        </Icon>
+      <div class="content">
+        <ul class="fredoka-reg has-text-justified {innerWidth < 571 ? 'txt-size-10' : 'txt-size-15'}">
+          <li>Uploading malicious code</li>
+          <li>Accessing data that you are not allowed to access</li>
+          <li>Sharing or displaying inappropriate, misleading information within Axion</li>
+        </ul>
       </div>
 
       <br/>
 
-      <p class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile has-text-justified">
+      <p class="fredoka-reg has-text-justified {innerWidth < 571 ? 'txt-size-10' : 'txt-size-15'}">
         We acquire the right to refuse our Service and terminate user accounts, if we believe that user has violated our discretion of rules.
       </p>
 
       <br/>
 
-      <p class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile has-text-justified has-text-weight-bold">
+      <p class="fredoka-reg has-text-justified {innerWidth < 571 ? 'txt-size-10' : 'txt-size-15'} has-text-weight-bold">
         Feedback statement
       </p>
-      <p class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile has-text-justified">
+      <p class="fredoka-reg has-text-justified {innerWidth < 571 ? 'txt-size-10' : 'txt-size-15'}">
         You may contact us and provide your feedback using our Contact Us page, regarding our Terms and Conditions, our Privacy Policy, or other matters that are not discussed and may need to be elaborated further.
       </p>
 
       <br/>
 
-      <p class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile has-text-justified has-text-weight-bold">
+      <p class="fredoka-reg has-text-justified {innerWidth < 571 ? 'txt-size-10' : 'txt-size-15'} has-text-weight-bold">
         Further updates in our terms and conditions
       </p>
-      <p class="dm-sans is-size-3-desktop is-size-3-tablet is-size-6-mobile has-text-justified">
+      <p class="fredoka-reg has-text-justified {innerWidth < 571 ? 'txt-size-10' : 'txt-size-15'}">
         In case we decide to change or update our terms and conditions, we will post them in here and update the effective date that is located above.
       </p>
     </div> 
